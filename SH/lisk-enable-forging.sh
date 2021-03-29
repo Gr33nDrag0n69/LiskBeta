4c13b63713a119762681660d134c02e4ac2668c7
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-03-29 10:54:34 -0400

    Update lisk-enable-forging.sh

--- SH/lisk-enable-forging.sh
+++ SH/lisk-enable-forging.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Gr33nDrag0n v1.0.1 (2021-03-27)
+# Gr33nDrag0n v1.1.0 (2021-03-29)
 
 ForgingStatus=$( lisk-core forging:status )
 
 BinaryAddress=$( echo $ForgingStatus | jq '.[0] | .address' | tr -d '"' )
+
 Height=$( echo $ForgingStatus | jq '.[0] | .height' )
+if [ "$Height" = "null" ]
+then
+      Height="0"
+fi
+
 MaxHeightPreviouslyForged=$( echo $ForgingStatus | jq '.[0] | .maxHeightPreviouslyForged' )
+if [ "$MaxHeightPreviouslyForged" = "null" ]
+then
+      MaxHeightPreviouslyForged="0"
+fi
+
 MaxHeightPrevoted=$( echo $ForgingStatus | jq '.[0] | .maxHeightPrevoted' )
+if [ "$MaxHeightPrevoted" = "null" ]
+then
+      MaxHeightPrevoted="0"
+fi
 
 echo "Command: lisk-core forging:enable $BinaryAddress $Height $MaxHeightPreviouslyForged $MaxHeightPrevoted"
 
