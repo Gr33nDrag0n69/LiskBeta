ad9b317377554d58c3313570db0e90003e9d88b8
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2021-04-04 23:43:53 -0400

    Update lisk-rebuild.sh

--- SH/lisk-rebuild.sh
+++ SH/lisk-rebuild.sh
@@ -3,6 +3,8 @@
 # Gr33nDrag0n v1.0.0 (2021-04-04)
 
 lisk-core blockchain:download --url https://betanet-snapshot.lisknode.io/blockchain.db.tar.gz --output ~/
+pm2 stop lisk-core --silent && sleep 3
 lisk-core blockchain:import ~/blockchain.db.tar.gz --force
+pm2 start ~/lisk-core.pm2.json --silent
 rm -f ~/blockchain.db.tar.gz
 rm -f ~/blockchain.db.tar.gz.SHA256
